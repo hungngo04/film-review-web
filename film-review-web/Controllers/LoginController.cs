da1fcf987140fe81275c356fc1b2f68e5ae6b27e
hungngo04 <ngotienhung195@gmail.com>
2022-07-30 16:52:51 +0700

    update

---
+++ film-review-web/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using film_review_web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace film_review_web.Controllers
+{
+    public class LoginController : Controller
+    {
+        string ConnectionString = ConfigurationManager.ConnectionStrings["MoviesDbEntities"].ConnectionString;
+        MoviesDbEntities db = new MoviesDbEntities();
+
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Verify(account acc)
+        {
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            com.Connection = conn;
+            com.CommandText = "select * from account where username='"+acc.username+"' and userpassword='"+acc.userpassword+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                conn.Close();
+                return View("Index");
+            }
+            else
+            {
+                conn.Close();
+                return View("Error");
+            }
+        }
+
+
+    }
+}
